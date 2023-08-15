e7c8ad79b9bb4c6df155538ba38f92cdb53761b4
jetblack <coolanimeguy666@gmail.com>
2023-08-15 13:01:20 -0600

    small changes

--- boot.asm
+++ boot.asm
@@ -14,15 +14,16 @@ init:
   mov si, msg  ; loads the address of "msg" into SI register
   mov ah, 0x0e ; sets AH to 0xe (function teletype)
 print_char:
-  lodsb     ; loads the current byte from SI into AL and increments the address in SI
+  mov al, [esi]
   cmp al, 0 ; compares AL to zero
-  je done   ; if AL == 0, jump to "done"
+  je done  
+  inc esi
   int 0x10  ; print to screen using function 0xe of interrupt 0x10
   jmp print_char ; repeat with next byte
 done:
   hlt ; stop execution
 
-msg: db "Hello world!", 0 ; we need to explicitely put the zero byte here
+msg: db "Hello Bootloader! ", 0 ; we need to explicitely put the zero byte here
 
 times 510-($-$$) db 0           ; fill the output file with zeroes until 510 bytes are full
 dw 0xaa55                       ; magic number that tells the BIOS this is bootable
--- makefile
+++ makefile
@@ -1,4 +1,3 @@
 run:
 	nasm  boot.asm -o boot.bin -f bin
-	sudo dd if=boot.bin bs=512 of=boot.img
-	qemu-system-x86_64 boot.img
+	qemu-system-x86_64 boot.bin
