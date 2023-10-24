3a5f6d942ad6e0bea35da61d8bade2eef4693b61
jetblack <coolanimeguy666@gmail.com>
2023-10-23 19:10:16 -0600

    boot 2 is boot looping :(

--- boot2.asm
+++ boot2.asm
@@ -1,4 +1,6 @@
 ;seek=1 count=1
+[bits 16]    ; use 16 bits
+
 [org 0x2000] ; sets the start address
 jmp main2
 
@@ -26,11 +28,72 @@ puts:
     ret
     
 main2:
-    mov si ,m_load
-    call puts
-    hlt
-    
+mov si, m_load
+call puts
+CODE_SEG equ GDT_code - GDT_start
+DATA_SEG equ GDT_data - GDT_start
+
+cli
+lgdt [GDT_descriptor]
+mov eax, cr0
+or eax, 1
+mov cr0, eax
+jmp CODE_SEG:start_protected_mode
+
+jmp $
+                                    
+                                     
+GDT_start:                          ; must be at the end of real mode code
+    GDT_null:
+        dd 0x0
+        dd 0x0
+
+    GDT_code:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10011010
+        db 0b11001111
+        db 0x0
+
+    GDT_data:
+        dw 0xffff
+        dw 0x0
+        db 0x0
+        db 0b10010010
+        db 0b11001111
+        db 0x0
+
+GDT_end:
+
+GDT_descriptor:
+    dw GDT_end - GDT_start - 1
+    dd GDT_start
 
 
+[bits 32]
+start_protected_mode:
+    mov ax, GDT_data
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    mov al, 'A'
+
+    mov ax, 0x0F41   ; BrightWhiteOnBlack 'A'
+    mov [0xb8000], ax
+
+
+    ; mov [0xb8000], ax
+    jmp $
+
+
+; start_protected_mode:
 
 m_load: db 'second sector!',0xa,0xd, 0 
+m_protected: db 'entering protected!',0xa,0xd, 0 
